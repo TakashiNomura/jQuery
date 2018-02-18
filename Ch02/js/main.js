34b89133abed64956180fa62a6613d3c9b8d2a83
nomufamily <nomufamily@gmail.com>
2018-02-18 17:13:34 +0900

    p.61

--- Ch02/js/main.js
+++ Ch02/js/main.js
@@ -2,8 +2,7 @@ $(function(){
     $('#typo .inner')
         .on('click', function(){
             $('#typo .inner').animate({
-                opacity: 0,
-                fontSize: '0px'    
+                color: '#ebc000'
             },
             1500
             );
