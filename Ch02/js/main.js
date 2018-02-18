3124335befae84d00ea3315a595c13f9af35c8f6
nomufamily <nomufamily@gmail.com>
2018-02-18 20:18:40 +0900

    p.67

--- Ch02/js/main.js
+++ Ch02/js/main.js
@@ -1,12 +1,12 @@
 $(function(){
     $('#typo .inner')
         .on('mouseover', function(){
-            $('#typo').animate({
+            $('#typo').stop(true).animate({
                 backgroundColor: '#ae5e9b'
             }, 500);
     })
     .on('mouseout', function(){
-        $('#typo').animate({
+        $('#typo').stop(true).animate({
             backgroundColor: '#3498db'
         }, 500);
     });
