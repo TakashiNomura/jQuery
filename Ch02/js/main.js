080c69f61f4b06d798af8dd6ff1f0427ef6e5b84
nomufamily <nomufamily@gmail.com>
2018-02-18 14:34:18 +0900

    p.50

--- Ch02/js/main.js
+++ Ch02/js/main.js
@@ -1,6 +1,8 @@
 $(function(){
     console.log($('#typo').css('color'));
-    $('#typo').css('font-size', '50px');
-    $('#typo').css('background-color', '#ae5e9b');
-    $('#typo').css('color', '#ebc000');
+    $('#typo').css({
+        'font-size': '50px',
+        'background-color': '#ae5e9b',
+        'color': '#ebc000'
+    })
 });
