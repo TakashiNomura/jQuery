5e57931435c39ef3bb791c7c78ec2ff797d2330d
nomufamily <nomufamily@gmail.com>
2018-02-18 14:45:16 +0900

    p.55

--- Ch02/js/main.js
+++ Ch02/js/main.js
@@ -1,8 +1,10 @@
 $(function(){
     $('#typo').on('mouseover', function(){
         $('#typo').css('color', '#ebc000');
+        $('header').css('color', '#ebc000');
     });
     $('#typo').on('mouseout', function(){
         $('#typo').css('color', '#ffffff');
+        $('header').css('color', '#000000');
     });
 });
